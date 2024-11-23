15c103222f633a6b117a1febba63ddf395a04a8d
Turganbay Myrzaliyev <t.myrzaliyev@alabs.team>
2024-11-24 02:19:40 +0600

    Добавьте файлы проекта.

---
+++ TestApiCurrency/BLL/RateUsdKztBll.cs
@@ -0,0 +1,60 @@
+﻿using HtmlAgilityPack;
+using Microsoft.Extensions.Configuration;
+using System.Text;
+
+namespace TestApiCurrency.BLL
+{
+    public class RateUsdKztBll
+    {
+        private string _currencyUrlPath = "";
+        private readonly HttpClient _httpClient;
+
+        public RateUsdKztBll(HttpClient client,IConfiguration configuration)
+        {
+            this._httpClient = client;
+            this._currencyUrlPath = configuration.GetValue<string>("AppSettings:CurrencyUrlPath") ?? "";
+        }
+
+        public async Task<decimal?> getRateUsdKzt()
+        {
+            try
+            {
+                //check is url is assigned
+                if (!String.IsNullOrEmpty(_currencyUrlPath))
+                {
+                    // Запрос на получение страницы
+                    var response = await _httpClient.GetStringAsync(_currencyUrlPath);                    
+
+                    var htmlDocument = new HtmlDocument();
+                    htmlDocument.LoadHtml(response);
+                    
+                    // Необходимо найти в таблице значение доллара используя XPath
+                    var node = htmlDocument.DocumentNode.SelectSingleNode("//td[contains(text(), 'USD')]/following-sibling::td");
+
+                    if (node != null)
+                    {
+                        string currency = node.InnerText.Trim();
+                        if (decimal.TryParse(currency, out decimal rate))
+                        {
+                            return rate;
+                        }
+                    }
+                    return null;
+                }
+                else
+                {
+                    Console.WriteLine("$Ошибка не найден путь к api");
+                    return null;
+                }
+            }
+            catch (Exception ex) { 
+                Console.WriteLine("$Ошибка: {0}", ex.Message);
+                return null;
+            }
+
+        }
+
+
+
+    }
+}
---
+++ TestApiCurrency/Controllers/CurrencyUsdKztController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using TestApiCurrency.BLL;
+
+namespace TestApiCurrency.Controllers
+{
+    [Route("api/rate_usd_kzt")]
+    [ApiController]
+    public class CurrencyUsdKztController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly RateUsdKztBll _rateCurrencyBll;
+
+        public CurrencyUsdKztController(RateUsdKztBll rateUsdKztBll, IConfiguration configuration)
+        {
+            this._rateCurrencyBll = rateUsdKztBll;
+            this._configuration = configuration;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var rateVal = await _rateCurrencyBll.getRateUsdKzt();
+
+            if (rateVal.HasValue)
+            {
+                return Ok(rateVal.Value);
+            }
+            else
+            {
+                return NotFound(new { error = "Не удалось получить курс валют" });
+            }
+
+        }
+    }
+}
